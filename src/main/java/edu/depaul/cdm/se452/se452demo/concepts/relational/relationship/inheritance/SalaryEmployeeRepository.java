6dc7312521874abd8df39fc6552981536c04d7b0
Ken Yu <kyu@cdm.depaul.edu>
2023-04-11 05:56:59 -0500

    remove relationship

--- src/main/java/edu/depaul/cdm/se452/se452demo/concepts/relational/relationship/inheritance/SalaryEmployeeRepository.java
+++
@@ -1,5 +0,0 @@
-package edu.depaul.cdm.se452.se452demo.concepts.relational.relationship.inheritance;
-
-public interface SalaryEmployeeRepository extends EmployeeRepository<SalaryEmployee> {
-
-}
--- src/main/java/edu/depaul/cdm/se452/se452demo/concepts/relational/relationship/inheritance/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Example to show persistence and interitance 
- */
-package edu.depaul.cdm.se452.se452demo.concepts.relational.relationship.inheritance;
