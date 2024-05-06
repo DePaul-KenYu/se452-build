7158f90e8dfa165a5dac1bf8153ce6595e221cea
Ken Yu <kyu@cdm.depaul.edu>
2024-05-06 18:26:49 -0500

    updating to show build working

--- src/main/java/edu/depaul/cdm/se452/se452demo/App.java
+++ src/main/java/edu/depaul/cdm/se452/se452demo/App.java
@@ -22,7 +22,7 @@ public class App {
 	public CommandLineRunner showLogLevel() {
 		return (args) -> {
 			System.out.println(env);
-			log.debug("Debug2");
+			log.debug("Debug");
 			log.info("Info");
 			log.warn("Warning");
 			log.error("Error");
