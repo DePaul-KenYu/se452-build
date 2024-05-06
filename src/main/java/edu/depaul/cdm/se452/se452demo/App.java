bf6bb091c73cc7ae9135a676954e2e9b96244a07
Ken Yu <kyu@cdm.depaul.edu>
2024-05-06 18:31:19 -0500

    forcing a build error

--- src/main/java/edu/depaul/cdm/se452/se452demo/App.java
+++ src/main/java/edu/depaul/cdm/se452/se452demo/App.java
@@ -22,6 +22,7 @@ public class App {
 	public CommandLineRunner showLogLevel() {
 		return (args) -> {
 			System.out.println(env);
+			Oops
 			log.debug("Debug");
 			log.info("Info");
 			log.warn("Warning");
